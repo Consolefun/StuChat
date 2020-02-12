f850e35d33ccf37b482a24c028b848f435b0edcf
William Hoang <46702069+Consolefun@users.noreply.github.com>
2020-02-12 16:18:16 -0600

    Update APINotification.java

--- animeChat_v001/app/src/main/java/com/example/lab2_mobiledevelopment/APINotification.java
+++ animeChat_v001/app/src/main/java/com/example/lab2_mobiledevelopment/APINotification.java
@@ -14,7 +14,7 @@ public interface APINotification {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAQ9JOwuI:APA91bGgIoXQsn58CtsC7uWjDLGnk9FENd3BWFgtZZIUWPZG-G21Ox7-yS4JbfQN-vkGO-2RR1wKcx2rvHjTOMGui0dEXmg6MpxHFnRCJDtxEqVf0jHXbxRq3sp96VgaQ-UVQNJBqMKt"
+                    "Authorization:key=Enter your Key"
             }
     )
 
